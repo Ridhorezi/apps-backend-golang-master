08457fbb7bb371bc28c6c725a50142d24e44fc56
Ridho Suhaebi Arrowi <ridhorezi1212@gmail.com>
2022-08-12 15:13:53 +0700

    get notification transaction endpoint

--- transaction/repository.go
+++ transaction/repository.go
@@ -7,6 +7,7 @@ import (
 type Repository interface {
 	GetByCampaignID(campaignID int) ([]Transaction, error)
 	GetByUserID(userID int) ([]Transaction, error)
+	GetByID(ID int) (Transaction, error)
 	Save(transaction Transaction) (Transaction, error)
 	Update(transaction Transaction) (Transaction, error)
 }
@@ -47,6 +48,20 @@ func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
 	}
 
 	return transactions, nil
+
+}
+
+func (r *repository) GetByID(ID int) (Transaction, error) {
+
+	var transaction Transaction
+
+	err := r.db.Where("id = ?", ID).Find(&transaction).Error
+
+	if err != nil {
+		return transaction, err
+	}
+
+	return transaction, nil
 
 }
 
